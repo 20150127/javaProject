7993da8319fc7b3a2480f84322b8ee4d951c236c
cumtsw <sw.cumt.1993@gmail.com>
2015-01-27 10:56:52 +0800

    【创建】

---
+++ zs/src/com/sw/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sw;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
